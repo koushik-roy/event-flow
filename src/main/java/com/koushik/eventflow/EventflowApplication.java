12cffd39e8c7d98885fe2b947971113b0642e732
Koushik Roy <jc.koushikroy@gmail.com>
2026-08-18 22:58:45 +0530

    Added springboot files

---
+++ src/main/java/com/koushik/eventflow/EventflowApplication.java
@@ -0,0 +1,13 @@
+package com.koushik.eventflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventflowApplication.class, args);
+	}
+
+}
